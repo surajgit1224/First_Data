7edaf30b065c273b505576bfed1ea46db10c0b33
Suraj.umbarkar <Suraj.umbarkar@SCS-R37.harbinger.in>
2019-09-09 18:02:36 +0530

    First Commit

---
+++ src/main/java/First_Data/First_Data/App.java
@@ -0,0 +1,13 @@
+package First_Data.First_Data;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
