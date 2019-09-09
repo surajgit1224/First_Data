6d69d652900022657fc8127fa06142382f627172
Suraj.umbarkar <Suraj.umbarkar@SCS-R37.harbinger.in>
2019-09-09 18:06:34 +0530

    First class commit

---
+++ src/test/java/First_Data/First_Data/First_Class.java
@@ -0,0 +1,12 @@
+package First_Data.First_Data;
+
+public class First_Class {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello Word");
+
+	}
+
+}
